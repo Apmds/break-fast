e7076ea471d3084d5945ef725d29a942abcd5462
Apmds <ap1mrfj@gmail.com>
2026-04-16 01:25:16 +0100

    gui update

--- src/city/city.js
+++ src/city/city.js
@@ -77,17 +77,18 @@ class City extends Scene {
         this.physicsWorld.addBody(this.groundBody);
 
         // GUI
-        this.gui.makeFolder('Camera Position');
-        this.gui.add('Camera Position', 'X', camera.position, 'x').listen();
-        this.gui.add('Camera Position', 'Y', camera.position, 'y').listen();
-        this.gui.add('Camera Position', 'Z', camera.position, 'z').listen();
+        this.gui.hide()
+        //this.gui.makeFolder('Camera Position');
+        //this.gui.add('Camera Position', 'X', camera.position, 'x').listen();
+        //this.gui.add('Camera Position', 'Y', camera.position, 'y').listen();
+        //this.gui.add('Camera Position', 'Z', camera.position, 'z').listen();
 
-        this.gui.makeFolder('Lighting');
+        //this.gui.makeFolder('Lighting');
         
-        this.gui.add('Lighting', 'key intensity', keyLight, 'intensity', 0, 5, 0.01);
-        this.gui.add('Lighting', 'fill intensity', fillLight, 'intensity', 0, 2, 0.01);
-        this.gui.add('Lighting', 'rim intensity', rimLight, 'intensity', 0, 2, 0.01);
-        this.gui.add('Lighting', 'hemi intensity', hemisphereLight, 'intensity', 0, 2, 0.01);
+        //this.gui.add('Lighting', 'key intensity', keyLight, 'intensity', 0, 5, 0.01);
+        //this.gui.add('Lighting', 'fill intensity', fillLight, 'intensity', 0, 2, 0.01);
+        //this.gui.add('Lighting', 'rim intensity', rimLight, 'intensity', 0, 2, 0.01);
+        //this.gui.add('Lighting', 'hemi intensity', hemisphereLight, 'intensity', 0, 2, 0.01);
     }
 
     update(delta) {
@@ -158,6 +159,86 @@ function make_city() {
         city.add(base_grass);
     }
 
+    // Hand-placed tree scatter: very dense in the main grass area and lighter in side patches.
+    {
+        const classicTreesMain = [
+            [483, -153, 0.9], [618, -411, 1.0], [373, -226, 1.1],
+            [585, -276, 0.8],
+            [498, -710, 0.8], [285, -675, 0.9], [538, -508, 1.0], [523, -676, 1.1],
+            [430, -730, 0.8], [520, -443, 1.0], [590, -305, 1.1],
+            [388, -600, 0.8], [613, -430, 0.9], [568, -260, 1.1],
+            [360, -495, 0.8], [448, -536, 0.9],
+            [423, -123, 0.8], [520, -395, 0.9], [475, -311, 1.1],
+            [398, -300, 0.8], [443, -620, 0.9], [438, -206, 1.0], [510, -540, 1.1],
+            [393, -210, 1.0],
+            [408, -555, 0.8], [573, -581, 0.9], [388, -200, 1.1],
+            [508, -295, 0.8], [615, -523, 1.1],
+            [388, -438, 1.0], [463, -223, 1.1],
+            [645, -751, 0.9], [498, -435, 1.1],
+            [508, -291, 0.8], [273, -548, 0.9], [645, -416, 1.0],
+            [415, -613, 0.9], [418, -205, 1.0],
+            [333, -243, 0.9], [415, -541, 1.0], [310, -573, 1.1],
+            [413, -551, 0.8], [328, -563, 1.1],
+            [498, -301, 0.8], [433, -170, 1.1],
+            [603, -263, 0.8], [403, -303, 0.9], [558, -128, 1.1],
+            [298, -595, 0.9],
+            [390, -520, 0.8], [398, -571, 1.0], [560, -498, 1.1],
+        ];
+
+        const crownTreesMain = [
+            [328, -611, 0.9], [338, -451, 1.1], [558, -483, 0.9],
+            [470, -291, 1.0], [543, -453, 1.1], [448, -730, 0.9], [498, -463, 1.0],
+            [553, -760, 1.1], [548, -668, 1.0], [483, -381, 1.1],
+            [318, -748, 0.9], [303, -710, 1.0], [600, -235, 0.9],
+            [508, -455, 0.9], [315, -226, 1.0],
+            [478, -356, 1.1], [498, -126, 1.1],
+            [348, -146, 0.9], [640, -198, 1.1], [530, -303, 0.9],
+            [300, -320, 1.0], [523, -245, 1.1], [550, -601, 0.9],
+            [338, -416, 1.1],
+            [638, -471, 1.1], [360, -123, 0.9],
+            [430, -540, 1.1], [643, -706, 0.9],
+            [333, -376, 0.9], [273, -338, 0.9],
+            [630, -261, 1.0], [338, -216, 1.1], [580, -753, 0.9], [345, -661, 1.0],
+            [390, -466, 1.1], [448, -713, 0.9], [473, -610, 1.0],
+            [513, -606, 0.9], [435, -283, 1.1], [518, -278, 0.9],
+            [308, -445, 1.1], [353, -476, 0.9],
+            [325, -470, 0.9],
+            [305, -475, 0.9], [400, -596, 0.9],
+            [498, -121, 1.0],
+            [343, -716, 0.9],
+        ];
+
+        const classicTreesSide = [
+            [-138, -248, 0.8], [-104, -224, 0.9], [-78, -188, 1.0], [-126, -152, 1.1], [-92, -132, 0.8],
+            [-140, -618, 0.9], [-108, -582, 1.0], [-76, -540, 1.1], [-132, -470, 0.8], [-98, -402, 0.9],
+        ];
+
+        const crownTreesSide = [
+            [-122, -262, 0.9], [-88, -238, 1.0], [-70, -206, 1.1], [-136, -178, 0.9], [-106, -146, 1.0],
+            [-124, -632, 1.1], [-90, -596, 0.9], [-72, -560, 1.0], [-120, -500, 1.1], [-86, -430, 0.9],
+        ];
+
+        for (let i = 0; i < classicTreesMain.length; i++) {
+            const [x, z, scale] = classicTreesMain[i];
+            city.add(make_tree(x, 0, z, scale));
+        }
+
+        for (let i = 0; i < crownTreesMain.length; i++) {
+            const [x, z, scale] = crownTreesMain[i];
+            city.add(make_tree_crowns(x, 0, z, scale));
+        }
+
+        for (let i = 0; i < classicTreesSide.length; i++) {
+            const [x, z, scale] = classicTreesSide[i];
+            city.add(make_tree(x, 0, z, scale));
+        }
+
+        for (let i = 0; i < crownTreesSide.length; i++) {
+            const [x, z, scale] = crownTreesSide[i];
+            city.add(make_tree_crowns(x, 0, z, scale));
+        }
+    }
+
     let road_start;
 
     let [road, endpoint] = make_road_corner(0, 0, ROAD_CORNER_DIR.LEFT_UP, 50);
@@ -225,138 +306,7 @@ function make_city() {
 
     // TODO: estrada sem saída
 
-
-    // Trees - Main large grass area (x: 400, z: -300)
-    city.add(make_tree(250, 0, -100, 0.8));
-    city.add(make_tree_crowns(265, 0, -85, 0.9));
     
-    city.add(make_tree(350, 0, -50, 0.9));
-    city.add(make_tree_crowns(335, 0, -70, 1.0));
-    
-    city.add(make_tree(450, 0, 0, 1.0));
-    city.add(make_tree_crowns(475, 0, 20, 1.1));
-    
-    city.add(make_tree(550, 0, -150, 1.1));
-    city.add(make_tree_crowns(530, 0, -130, 0.9));
-    
-    city.add(make_tree(300, 0, -250, 0.8));
-    city.add(make_tree_crowns(320, 0, -270, 1.0));
-    
-    city.add(make_tree(500, 0, -300, 0.9));
-    city.add(make_tree_crowns(515, 0, -280, 1.1));
-    
-    city.add(make_tree(650, 0, -200, 1.0));
-    city.add(make_tree_crowns(630, 0, -220, 0.9));
-    
-    city.add(make_tree(200, 0, -400, 1.1));
-    city.add(make_tree_crowns(225, 0, -385, 1.0));
-    
-    city.add(make_tree(400, 0, -450, 0.8));
-    city.add(make_tree_crowns(385, 0, -430, 1.1));
-    
-    city.add(make_tree(600, 0, -500, 0.9));
-    city.add(make_tree_crowns(620, 0, -485, 0.9));
-    
-    city.add(make_tree(750, 0, -350, 1.0));
-    city.add(make_tree_crowns(735, 0, -370, 1.0));
-    
-    city.add(make_tree(150, 0, -550, 1.1));
-    city.add(make_tree_crowns(175, 0, -535, 0.9));
-    
-    city.add(make_tree(350, 0, -600, 0.8));
-    city.add(make_tree_crowns(370, 0, -580, 1.1));
-    
-    city.add(make_tree(550, 0, -700, 0.9));
-    city.add(make_tree_crowns(565, 0, -720, 1.0));
-    
-    city.add(make_tree(200, 0, 100, 1.0));
-    city.add(make_tree_crowns(180, 0, 80, 0.9));
-    
-    city.add(make_tree(650, 0, 50, 1.1));
-    city.add(make_tree_crowns(675, 0, 30, 1.0));
-    
-    city.add(make_tree(800, 0, -100, 0.8));
-    city.add(make_tree_crowns(820, 0, -120, 1.1));
-
-    city.add(make_tree(100, 0, 0, 0.9));
-    city.add(make_tree_crowns(120, 0, 15, 1.0));
-
-    city.add(make_tree(280, 0, 50, 1.0));
-    city.add(make_tree_crowns(260, 0, 70, 0.9));
-
-    city.add(make_tree(420, 0, 100, 1.1));
-    city.add(make_tree_crowns(440, 0, 120, 1.0));
-
-    city.add(make_tree(700, 0, 0, 0.8));
-    city.add(make_tree_crowns(720, 0, -15, 1.1));
-
-    city.add(make_tree(850, 0, 50, 0.9));
-    city.add(make_tree_crowns(830, 0, 35, 1.0));
-
-    city.add(make_tree(180, 0, -200, 1.0));
-    city.add(make_tree_crowns(200, 0, -185, 0.9));
-
-    city.add(make_tree(380, 0, -150, 1.1));
-    city.add(make_tree_crowns(360, 0, -165, 1.0));
-
-    city.add(make_tree(680, 0, -100, 0.8));
-    city.add(make_tree_crowns(700, 0, -80, 1.1));
-
-    city.add(make_tree(750, 0, -500, 0.9));
-    city.add(make_tree_crowns(770, 0, -515, 1.0));
-
-    city.add(make_tree(250, 0, -350, 1.0));
-    city.add(make_tree_crowns(235, 0, -365, 0.9));
-
-    city.add(make_tree(550, 0, -400, 1.1));
-    city.add(make_tree_crowns(535, 0, -385, 1.0));
-
-    city.add(make_tree(100, 0, -500, 0.8));
-    city.add(make_tree_crowns(120, 0, -520, 1.1));
-
-    city.add(make_tree(450, 0, -550, 0.9));
-    city.add(make_tree_crowns(470, 0, -535, 1.0));
-
-    city.add(make_tree(800, 0, -400, 1.0));
-    city.add(make_tree_crowns(785, 0, -420, 0.9));
-
-    city.add(make_tree(300, 0, -700, 1.1));
-    city.add(make_tree_crowns(320, 0, -685, 1.0));
-
-    city.add(make_tree(700, 0, -650, 0.8));
-    city.add(make_tree_crowns(715, 0, -670, 1.1));
-
-    city.add(make_tree(150, 0, -150, 0.9));
-    city.add(make_tree_crowns(135, 0, -135, 1.0));
-
-    city.add(make_tree(500, 0, -50, 1.0));
-    city.add(make_tree_crowns(520, 0, -65, 0.9));
-
-    // Trees - Left small grass area (x: -106, z: -190)
-    city.add(make_tree(-106, 0, -150, 0.9));
-    city.add(make_tree_crowns(-106, 0, -150, 1.0));
-    
-    city.add(make_tree(-75, 0, -200, 0.8));
-    city.add(make_tree_crowns(-75, 0, -200, 0.9));
-    
-    city.add(make_tree(-140, 0, -220, 1.0));
-    city.add(make_tree_crowns(-140, 0, -220, 1.1));
-
-    // Trees - Left lower grass area (x: -106, z: -500)
-    city.add(make_tree(-80, 0, -400, 0.9));
-    city.add(make_tree_crowns(-80, 0, -400, 1.0));
-    
-    city.add(make_tree(-130, 0, -450, 1.1));
-    city.add(make_tree_crowns(-130, 0, -450, 0.9));
-    
-    city.add(make_tree(-60, 0, -550, 0.8));
-    city.add(make_tree_crowns(-60, 0, -550, 1.1));
-    
-    city.add(make_tree(-140, 0, -600, 1.0));
-    city.add(make_tree_crowns(-140, 0, -600, 1.0));
-    
-    city.add(make_tree(-90, 0, -700, 0.9));
-    city.add(make_tree_crowns(-90, 0, -700, 0.9));
 
     // Houses
     {
