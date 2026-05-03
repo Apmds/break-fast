9920583b7c5c10b7320f44b34333c5503c83844f
Apmds <ap1mrfj@gmail.com>
2026-05-03 09:49:04 +0100

    bridge dialogue with delays

--- src/utils/dialogue_map.js
+++ src/utils/dialogue_map.js
@@ -13,31 +13,61 @@ const dialogueMap = {
             {"speaker": "Construction Worker", "text": [
                 {"text": "Hey, ", delay: 0.1},
                 {"text": " the bridge is still under maintenance.", delay: 0.2},
-                " You can't pass."
+                " You can't pass.",
             ]},
             {"speaker": "Me", "text": [
                 {"text": "Why?", delay: 0.2},
-                " Wasn't the it supposed to end today?"
+                " Wasn't the it supposed to end today?",
             ]},
-            {"speaker": "Construction Worker", "text": "Yes, and we actually just ended the job, but the boss still needs to give the final aproval before the bridge is usable again."},
-            {"speaker": "Me", "text": "Phew, that's a relief... For a second I thought my vacation time was being wasted."},
-            {"speaker": "Me", "text": "Where is the boss then? Tell him to hurry up."},
-            {"speaker": "Construction Worker", "text": "We can't find him right now, he's somewhere else."},
-            {"speaker": "Me", "text": "...", sound: false},
+            {"speaker": "Construction Worker", "text": [
+                {"text": "Yes", delay: 0.2},
+                {"text": ", and we actually just ended the job", delay: 0.2},
+                ", but the boss still needs to give the final aproval before the bridge is usable again.",
+            ]},
+            {"speaker": "Me", "text": [
+                {"text": "Phew", delay: 0.2},
+                {"text": ", that's a relief...", delay: 0.4},
+                "For a second I thought my vacation time was being wasted.",
+            ]},
+            {"speaker": "Me", "text": [
+                {"text": "Where is the boss then?", delay: 0.2},
+                " Tell him to hurry up.",
+            ]},
+            {"speaker": "Construction Worker", "text": [
+                {"text": "We can't find him right now", delay: 0.1},
+                ", he's somewhere else.",
+            ]},
+            {"speaker": "Me", "text": [
+                {"text": ".", delay: 0.5},
+                {"text": ".", delay: 0.5},
+                {"text": "."},                
+            ], sound: false},
             {"speaker": "Me", "text": "WHAT?"},
             {"speaker": "Me", "text": "WHAT DO YOU MEAN YOU DON'T KNOW WHERE HE IS?"},
             {"speaker": "Me", "text": "ISN'T HE SUPPOSED TO BE MANAGING THIS ALL??"},
             {"speaker": "Me", "text": "DO YOU KNOW HOW MUCH TIME I'VE BEEN WAITING FOR THIS VACATION???"},
             {"speaker": "Construction Worker", "text": [
-                {"text": "Calm down bro", delay: 0.1},
+                {"text": "Calm down bro", delay: 0.2},
                 ", yelling isn't going to fix anything."
             ]},
-            {"speaker": "Construction Worker", "text": "He's probably out there in the city. If you're in such a hurry to leave, go find him."},
-            {"speaker": "Me", "text": "Can't you call his phone? Surely he has one-", autoskip: true},
+            {"speaker": "Construction Worker", "text": [
+                {"text": "He's probably out there in the city.", delay: 0.2},
+                " If you're in such a hurry to leave, go find him.",
+            ]},
+            {"speaker": "Me", "text": [
+                {"text": "Can't you call his phone?", delay: 0.4},
+                {"text": " Surely he has one-", delay: 0.3},
+            ], autoskip: true},
             {"speaker": "Construction Worker", "text": "He doesn't have a phone. He insists on communicating by carrier pigeon."},
             {"speaker": "Me", "text": "You have to be kidding."},
-            {"speaker": "Construction Worker", "text": "No, he is just that guy."},
-            {"speaker": "Construction Worker", "text": "Now go away, you are becoming noise pollution."},
+            {"speaker": "Construction Worker", "text": [
+                {"text": "No,", delay: 0.2},
+                " he is just that guy.",
+            ]},
+            {"speaker": "Construction Worker", "text": [
+                {"text": "Now go away,", delay: 0.2},
+                " you are becoming noise pollution.",
+            ]},
         ],
     }
 };
